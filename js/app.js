ad1db5cdba053dd7879b47726e0f1d0dbd2af0bf
FirozHossain82 <firozhossain295@gmail.com>
2022-10-17 15:32:42 +0600

    all news deataile half added

--- js/app.js
+++ js/app.js
@@ -26,10 +26,31 @@ const loadAllNews = (category_id) => {
   const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayAllNews(data.data));
+    .then((data) => displayAllNews(data.data))
+    .catch((error) => console.log(error));
 };
 
-const displayAllNews = (AllNews) => {
-  console.log(AllNews);
+const displayAllNews = (allNews) => {
+  //   console.log(AllNews);
+  const allNewsContainer = document.getElementById("news-details");
+  allNewsContainer.textContent = "";
+
+  allNews.forEach((allnewses) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+        <div class="card lg:card-side bg-base-100  shadow-lg shadow-cyan-500/50 mx-auto my-6">
+          <figure><img src="${allnewses.thumbnail_url}" alt="Album"/></figure>
+            <div class="card-body">
+                 <h2 class="card-title"></h2>
+                  <p>Click the button to listen on Spotiwhy app.</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary">Listen</button>
+                    </div>
+            </div>
+        </div>
+    `;
+    allNewsContainer.appendChild(div);
+  });
 };
+
 lodeAllNewsCategory();
