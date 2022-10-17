37806ea1d087c8d71a2c0382469ef581af56f756
FirozHossain82 <firozhossain295@gmail.com>
2022-10-17 16:37:16 +0600

    all news deatails commit

--- js/app.js
+++ js/app.js
@@ -41,11 +41,37 @@ const displayAllNews = (allNews) => {
         <div class="card lg:card-side bg-base-100  shadow-lg shadow-cyan-500/50 mx-auto my-6">
           <figure><img src="${allnewses.thumbnail_url}" alt="Album"/></figure>
             <div class="card-body">
-                 <h2 class="card-title"></h2>
-                  <p>Click the button to listen on Spotiwhy app.</p>
-                    <div class="card-actions justify-end">
-                        <button class="btn btn-primary">Listen</button>
-                    </div>
+                 <h2 class="card-title  font-semibold text-white">${
+                   allnewses.title
+                 }</h2>
+                 <p class="text-gray-400">
+                  ${allnewses.details.slice(0, 300)}...</p>
+                 <div class="flex justify-between items-center">
+                        <div class="flex items-center">
+                            <figure><img class='w-12  px-2' src=${
+                              allnewses.author.img
+                            } alt="Album"></figure>
+                            <p>${
+                              allnewses.author.name
+                                ? allnewses.author.name
+                                : "Not A Found Result"
+                            }</p>
+                        </div>
+                        <div class="flex  items-center">
+                            <i class="fa-solid fa-eye">${
+                              allnewses.total_view
+                                ? allnewses.total_view
+                                : "no views this news"
+                            }</i>
+                        </div>
+                      <div class="card-actions">
+                          <button onclick="loadNewsDetails('${
+                            allnewses._id
+                          }')" class="btn btn-primary">Details</button>
+                      </div>
+                  </div>
+
+                   
             </div>
         </div>
     `;
