65a23582f0edc57c4a705f51fd8bc48e251c40a5
FirozHossain82 <firozhossain295@gmail.com>
2022-10-17 13:07:14 +0600

    All News category Added

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
+const lodeAllNewsCategory = () => {
+  const url = `https://openapi.programming-hero.com/api/news/categories`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayAllNewsCategory(data.data.news_category))
+    .catch((error) => console.log(error));
+};
 
+const displayAllNewsCategory = (newses) => {
+  //   console.log(newses);
+  const allNewsCategoryContainer = document.getElementById("all-news-category");
+  allNewsCategoryContainer.textContent = "";
+  newses.forEach((news) => {
+    const newsDiv = document.createElement("div");
+    newsDiv.innerHTML = `
+        <div class="font-bold text-fuchsia-800 text-center text-lg  ">
+           <button onclick=" loadAllNews('${news.category_id}')">${news.category_name}</button>
+        </div>
+    `;
+
+    allNewsCategoryContainer.appendChild(newsDiv);
+  });
+};
+lodeAllNewsCategory();
